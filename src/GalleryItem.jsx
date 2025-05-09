212629bd81346921e5617839ac4109b37460978f
atendale <112509840+atendale@users.noreply.github.com>
2025-05-09 17:58:07 -0400

    gallery display attempt

---
+++ src/GalleryItem.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import EventData from "./EventData.json"
+
+function GalleryItem(id) {
+    //   const [count, setCount] = useState(0)
+    eventItem = EventData[id];
+    
+
+    return (
+        <>
+            <div></div>
+        </>
+    )
+}
+
+export default Home
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 // import './Home.css'
+import EventData from "./EventData.json"
 
 function Home() {
     //   const [count, setCount] = useState(0)
@@ -10,7 +11,34 @@ function Home() {
             <h1>Chivent</h1>
             <h2>Upcoming Events:</h2>
             <div className="gallery">
-                {/* INSERT GALLERY JSX  */}
+            {EventData.length > 0 ? (
+                    EventData.map((events) => (
+                        <div key={EventData._id} className="event">
+                            <h3>{EventData.title}</h3>
+                            <img
+                                src={EventData.imageURL}
+                                alt={EventData.title}
+                                className="event-image"
+                            />
+                            <div className="event-data">
+                                <div className="start-date">
+                                    <h4>{EventData.startDate}</h4>
+                                </div>
+                                <div className="location">
+                                    <h4>{EventData.stats[1]}</h4>
+                                </div>
+                                <div className="stat">
+                                    <h5>{EventData.startTime} to {EventData.endTime}</h5>
+                                </div>
+                                <div className="price">
+                                    <h5> &#36; {EventData.price}</h5> {/*&#36; if $ not working*/}
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <p>No Events found</p>
+                )}
             </div>
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
