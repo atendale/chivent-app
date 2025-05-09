0a8f681a9010dedb65f3fad4fdf6e6b5a74cbf82
atendale <112509840+atendale@users.noreply.github.com>
2025-05-09 18:21:55 -0400

    Gallery data retrieval functional (images to be added)

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,28 +11,31 @@ function Home() {
             <h1>Chivent</h1>
             <h2>Upcoming Events:</h2>
             <div className="gallery">
-            {EventData.length > 0 ? (
-                    EventData.map((events) => (
-                        <div key={EventData._id} className="event">
-                            <h3>{EventData.title}</h3>
+                {EventData.length > 0 ? (
+                    EventData.map((event) => (
+                        <div key={event.id} className="event">
                             <img
-                                src={EventData.imageURL}
-                                alt={EventData.title}
+                                src={event.imageUrl}
+                                alt={event.title}
                                 className="event-image"
                             />
+                            <h3>{event.title}</h3>
+                
+                            <h4>{event.description}</h4>
                             <div className="event-data">
                                 <div className="start-date">
-                                    <h4>{EventData.startDate}</h4>
+                                    <h4>{event.startDate}</h4>
                                 </div>
                                 <div className="location">
-                                    <h4>{EventData.stats[1]}</h4>
+                                    <h4>{event.location}</h4>
                                 </div>
-                                <div className="stat">
-                                    <h5>{EventData.startTime} to {EventData.endTime}</h5>
+                                <div className="times">
+                                    <h5>{event.startTime} to {event.endTime}</h5>
                                 </div>
                                 <div className="price">
-                                    <h5> &#36; {EventData.price}</h5> {/*&#36; if $ not working*/}
+                                    <h5> &#36; {event.price}</h5> {/*&#36; if $ not working*/}
                                 </div>
+                            
                             </div>
                         </div>
                     ))
@@ -40,9 +43,6 @@ function Home() {
                     <p>No Events found</p>
                 )}
             </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
         </>
     )
 }
